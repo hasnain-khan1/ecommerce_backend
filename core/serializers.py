4eaeb70bd41508c6f18599a4d32024c39379a02b
Raheel Jadoon <raheeljadoon56@gmail.com>
2023-11-09 21:57:01 +0500

    Serializer Done

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product, Cart, CartItem, Checkout
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class CheckoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Checkout
         fields = '__all__'
