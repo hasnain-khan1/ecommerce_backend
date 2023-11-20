ea515025942159a0cde2f1b3436c595e77902cb4
Raheel Jadoon <rahee.riaz@hypernymbiz.com>
2023-11-20 11:33:46 +0500

    Product variation and attributes added

--- core/models.py
+++ core/models.py
@@ -19,14 +19,6 @@ class Category(LogsMixin):
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
 
-    def delete(self, using=None, keep_parents=False):
-        """
-        Override the delete method to update the 'status' field instead of hard delete.
-        """
-        self.status = StatusChoices.DELETED  # Update the 'status' field to your desired value
-        self.deleted_at = timezone.now()
-        self.save()
-
     def __str__(self):
         return self.name
 
@@ -41,16 +33,21 @@ class Product(LogsMixin):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sales_percentage = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
 
-    def delete(self, using=None, keep_parents=False):
-        """
-        Override the delete method to update the 'status' field instead of hard delete.
-        """
-        self.status = StatusChoices.DELETED  # Update the 'status' field to your desired value
-        self.deleted_at = timezone.now()
-        self.save()
-
     def __str__(self):
         return self.name
+
+
+class ProductVariation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variations")
+    color = models.CharField(max_length=50, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    size = models.CharField(max_length=25, null=True, blank=True)
+
+
+class ProductAttribute(models.Model):
+    product_variation = models.ForeignKey(ProductVariation, on_delete=models.CASCADE, related_name="attributes")
+    attribute_name = models.CharField(max_length=50, null=True)
+    attribute_value = models.CharField(max_length=100, blank=True)
 
 
 class Review(LogsMixin):
@@ -65,14 +62,6 @@ class Checkout(LogsMixin):
     shipping_address = models.TextField()
     payment_method = models.CharField(max_length=100)
 
-    def delete(self, using=None, keep_parents=False):
-        """
-        Override the delete method to update the 'status' field instead of hard delete.
-        """
-        self.status = StatusChoices.DELETED  # Update the 'status' field to your desired value
-        self.deleted_at = timezone.now()
-        self.save()
-
     def __str__(self):
         return f"{self.cart}-{self.cart}-checkout"
 
@@ -80,14 +69,6 @@ class Checkout(LogsMixin):
 class Cart(LogsMixin):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True, through='CartItem')
-
-    def delete(self, using=None, keep_parents=False):
-        """
-        Override the delete method to update the 'status' field instead of hard delete.
-        """
-        self.status = StatusChoices.DELETED  # Update the 'status' field to your desired value
-        self.deleted_at = timezone.now()
-        self.save()
 
     def __str__(self):
         return f"Cart for {self.user}"
@@ -98,28 +79,12 @@ class CartItem(LogsMixin):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)  # You can track the quantity of each product in the cart
 
-    def delete(self, using=None, keep_parents=False):
-        """
-        Override the delete method to update the 'status' field instead of hard delete.
-        """
-        self.status = StatusChoices.DELETED  # Update the 'status' field to your desired value
-        self.deleted_at = timezone.now()
-        self.save()
-
     def __str__(self):
         return f"{self.quantity} x {self.product.name} in cart"
 
 
 class BuyProduct(LogsMixin):
     checkout = models.ForeignKey(Checkout, on_delete=models.CASCADE, related_name="product_checkout")
-
-    def delete(self, using=None, keep_parents=False):
-        """
-        Override the delete method to update the 'status' field instead of hard delete.
-        """
-        self.status = StatusChoices.DELETED  # Update the 'status' field to your desired value
-        self.deleted_at = timezone.now()
-        self.save()
 
     def __str__(self):
         return f"{self.checkout}"
